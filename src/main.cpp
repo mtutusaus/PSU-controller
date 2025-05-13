3b8e8b72be2f8726ab39cd2adab0d6c8b9e1a286
Miquel Tutusaus <149240218+mtutusaus@users.noreply.github.com>
2025-05-13 20:01:55 +0200

    added carry/borrow feature in current display

--- src/main.cpp
+++ src/main.cpp
@@ -18,43 +18,48 @@ uint16_t CLK_PIN_state;
 #define BTN_R_PIN 33
 #define BTN_OUT_PIN 27
 
+// CURSOR POSITION
 int curr_col;
 int curr_row;
-// i.e. 12.3 V, 1 is voltage_disp_1, 2 is _2 and 3 is _3
+
+// VOLTAGE DISPLAY (i.e. 12.3 V, 1 is voltage_disp_1, 2 is _2 and 3 is _3)
 int voltage_disp_1 = 0;
 int voltage_disp_2 = 0;
 int voltage_disp_3 = 0;
 
-// i.e. 250 mA, 2 is current_disp_1, 5 is _2 and 0 is _3
+// CURRENT DISPLAY (i.e. 250 mA, 2 is current_disp_1, 5 is _2 and 0 is _3)
 int current_disp_1 = 0;
 int current_disp_2 = 0;
 int current_disp_3 = 0;
 
-// float voltage_DAC = 0.0; // desired voltage at the DAC output
-// uint8_t DAC_bits; // voltage_DAC converter to a 8 bits value (DAC_scaled rounded)
-
-// to toggle output state depending on previous state
+// OUTPUT STATE
 bool output_state = false;
 
-// set the LCD address to 0x27 for a 20 chars and 4 line display
-LiquidCrystal_I2C lcd(0x27,20,4);  
+// DIGIT BLINKING
+unsigned long last_blink_time = 0;
+bool blink_state = false;
+bool last_blink_state = false;
+const unsigned long blink_interval = 500; // in  ms
 
+// LCD DEFINITION (library: LiquidCrystal_I2C@^1.1.4)
+LiquidCrystal_I2C lcd(0x27,20,4); // set the LCD address to 0x27 for a 20 chars and 4 line display
+
+// PUSH BUTTONS INITIALIZATION (library: ezButton@^1.0.6)
 ezButton BTN_L(BTN_L_PIN);
 ezButton BTN_R(BTN_R_PIN);
 ezButton BTN_OUT(BTN_OUT_PIN);
 ezButton SW(SW_PIN);
 
-
-
 void setup() {
   Serial.begin(9600);
-  // Button debounce time setting
+
+  // Button debounce time setting in ms
   BTN_L.setDebounceTime(50);
   BTN_R.setDebounceTime(50);
   BTN_OUT.setDebounceTime(50);
   SW.setDebounceTime(50);
 
-  // Input pins config
+  // Input pins configuration
   pinMode(CLK_PIN,INPUT);
   pinMode(DT_PIN,INPUT);
   
@@ -63,6 +68,7 @@ void setup() {
 }
 
 void loop() {
+  // Push button check state loop
   BTN_L.loop();
   BTN_R.loop();
   BTN_OUT.loop();
@@ -72,8 +78,8 @@ void loop() {
   CLK_PIN_state = (CLK_PIN_state<<1) | digitalRead(CLK_PIN) | 0xe000; // detects stable falling edge
   if (CLK_PIN_state == 0xf000){
     CLK_PIN_state = 0x0000;
-    if (curr_row == 1){ // VOLTAGE
-      if (digitalRead(DT_PIN)){ // CW
+    if (curr_row == 1){ // VOLTAGE ROW
+      if (digitalRead(DT_PIN)){ // DIRECTION CW
         switch(curr_col){
           case 0: voltage_disp_1++; break;
           case 1: voltage_disp_2++; break;
@@ -81,7 +87,7 @@ void loop() {
           default: break;
         }
       }
-      else{
+      else{ // DIRECTION CCW
         switch(curr_col){
           case 0: voltage_disp_1--; break;
           case 1: voltage_disp_2--; break;
@@ -103,26 +109,70 @@ void loop() {
       lcd.print(voltage_disp_3);
       lcd.setCursor(curr_col,curr_row);
     }
-    else{ // CURRENT
-      if (digitalRead(DT_PIN)){ // CW
-        switch(curr_col){
-          case 0: current_disp_1++; break;
-          case 1: current_disp_2++; break;
-          case 2: current_disp_3++; break;
-          default: break;
+    else{ // CURRENT ROW
+      if (digitalRead(DT_PIN)){ // DIRECTION CW
+        switch (curr_col) {
+          case 2:
+            current_disp_3++;
+            if (current_disp_3 > 9) {
+              current_disp_3 = 0;
+              current_disp_2++;
+              curr_col = 1;
+              if (current_disp_2 > 9) {
+                current_disp_2 = 0;
+                current_disp_1++;
+                curr_col = 0;
+              }
+            }
+            break;
+      
+          case 1:
+            current_disp_2++;
+            if (current_disp_2 > 9) {
+              current_disp_2 = 0;
+              current_disp_1++;
+              curr_col = 0;
+            }
+            break;
+      
+          case 0:
+            current_disp_1++;
+            break;
         }
       }
-      else{
-        switch(curr_col){
-          case 0: current_disp_1--; break;
-          case 1: current_disp_2--; break;
-          case 2: current_disp_3--; break;
-          default: break;
+      else{ // DIRECTION CCW
+        switch (curr_col) {
+          case 0:
+            current_disp_1--;
+            if (current_disp_1 < 0) {
+              current_disp_1 = 0;
+              current_disp_2--;
+              curr_col = 1;
+              if (current_disp_2 < 0) {
+                current_disp_2 = 0;
+                current_disp_1--;
+                curr_col = 2;
+              }
+            }
+            break;
+      
+          case 1:
+            current_disp_2--;
+            if (current_disp_2 < 0) {
+              current_disp_2 = 0;
+              current_disp_3--;
+              curr_col = 2;
+            }
+            break;
+      
+          case 2:
+            current_disp_3--;
+            break;
         }
       }
       // UPDATE CURRENT DISPLAY
       if (current_disp_1<0) {current_disp_1=0;}
-      if (current_disp_1>9) {current_disp_1=9;}
+      if (current_disp_1>5) {current_disp_1=5;}
       if (current_disp_2<0) {current_disp_2=0;}
       if (current_disp_2>9) {current_disp_2=9;}
       if (current_disp_3<0) {current_disp_3=0;}
@@ -135,15 +185,15 @@ void loop() {
     }
   }
 
-
   if(SW.isPressed()){
     switch(curr_row){
-      case 1: curr_row = 2; break;
-      case 2: curr_row = 1; break;
+      case 1: curr_row = 2; break; // move from voltage row to current's
+      case 2: curr_row = 1; break; // move from current row to voltage's
     }
     lcd.setCursor(curr_col,curr_row);
   }
-  // MOVE CURSOR
+
+  // MOVE CURSOR TO THE RIGHT
   if(BTN_R.isPressed())
   {
     curr_col++;
@@ -151,6 +201,8 @@ void loop() {
     lcd.setCursor(curr_col,curr_row);
     lcd.cursor();
   }
+
+  // MOVE CURSOR TO THE LEFT
   if(BTN_L.isPressed())
   {
     curr_col--;
